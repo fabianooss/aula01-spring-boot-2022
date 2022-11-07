aa78a501b98a2f6362212ca495d79f97ef2ec908
fabiano.oss <fabiano.oss@gmail.com>
2022-11-07 19:43:13 -0300

    Parameter

--- src/main/java/org/senac/aula01/api/CarroController.java
+++ src/main/java/org/senac/aula01/api/CarroController.java
@@ -3,6 +3,9 @@ package org.senac.aula01.api;
 import org.senac.aula01.model.Carro;
 import org.senac.aula01.repository.CarroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,8 +26,9 @@ public class CarroController {
     }
 
     @GetMapping
-    public List<Carro> findAll() {
-        return repository.findAll();
+    public List<Carro> findAll(@RequestParam int page, @RequestParam int size) {
+        PageRequest p = PageRequest.of(page, size);
+        return repository.findAll(p);
     }
 
 }
